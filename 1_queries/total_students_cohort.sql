8b2800ec1db65c92cf87705f983ba7c36b34f07a
Kateryna <katerynabatrakova@gmail.com>
2020-07-20 20:00:32 +0000

    Join tables to get the  total duration of assignments for a particular student and cohort

--- 1_queries/total_students_cohort.sql
+++ 1_queries/total_students_cohort.sql
@@ -1,3 +1,9 @@
 SELECT COUNT (name)
 FROM students 
 WHERE cohort_id=1 OR cohort_id=2 OR cohort_id=3;
+
+SELECT students.name,  cohorts.name, students.start_date as student_start_date, cohorts.start_date as cohort_start_date
+FROM students 
+INNER JOIN cohorts ON cohort_id=cohorts.id
+WHERE students.start_date != cohorts.start_date
+ORDER BY cohort_start_date;

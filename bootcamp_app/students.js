a235ed7ecf33ce6c741d2e92a63b4d3ed9a77d99
Kateryna <katerynabatrakova@gmail.com>
2020-07-23 15:41:10 +0000

    Update code to protect from SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,25 +9,33 @@ const pool = new Pool({
 
 pool.connect()
 
+
+//The values that come from somewhere else:
 const cohortName = process.argv[2];
+const cohortLimit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, cohortLimit];
 
-// const [, , cohortName, cohortLimit] = process.argv
-// console.log(cohortName, '--- cohortName', cohortLimit, ' --- cohortLimit' )
-
-
-// pool.query is a function that accepts an SQL query as a JavaScript string
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${cohortLimit || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2; 
+`;
+// $ is a placeholder that represents where a value should go but can't as it might be malicious
+//$1 and $2 placeholders will eventually get replaced with the actual data from the values array
+
+
+// pool.query is a function that accepts an SQL query as a JavaScript string
+pool.query(queryString, values)
+  //queryString - safe ;  values - data that may be malicious
+  //It will use the values  within the query but  not run values as part of the query
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  }).catch(err => console.error('query error', err.stack));
 
 
 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,19 +9,28 @@ const pool = new Pool({
 
 pool.connect()
 
+//The values that come from somewhere else:
 const cohortName = process.argv[2];
-const query = `SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+// Store all potentially malicious values in an array
+const values = [`%${cohortName}%`];
+
+
+const queryString = `SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName}'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;`
+// $ is a placeholder that represents where a value should go but can't as it might be malicious
+//$1 and $2 placeholders will eventually get replaced with the actual data from the values array
 
-
-pool.query(query)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(` ${row.cohort_name}: ${row.teacher_name}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+// pool.query is a function that accepts an SQL query as a JavaScript string
+pool.query(queryString, values)
+  //queryString - safe ;  values - data that may be malicious
+  //It will use the values  within the query but not run values as part of the query
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(` ${row.cohort_name}: ${row.teacher_name}`);
+    })
+  }).catch(err => console.error('query error', err.stack));
